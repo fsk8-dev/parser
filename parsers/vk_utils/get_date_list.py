69b9465d5d12e586f53c79990dfef154a258be91
gen <kyliki@yandex.ru>
2025-01-12 22:12:03 +0300

    fix date pattern, fix call ArenaSchedule

--- parsers/vk_utils/get_date_list.py
+++ parsers/vk_utils/get_date_list.py
@@ -6,10 +6,13 @@ def get_date_list(text: str, period_pattern: str):
     date_list = []
     current_year = str(datetime.now().year)
     format_pattern = '%d.%m.%Y'
-    match = re.search(period_pattern, text)
+    match = re.search(period_pattern, text, re.IGNORECASE)
     if match:
-        end_date = datetime.strptime(match.group(3) + '.' + current_year, format_pattern)
-        start_date = datetime.strptime(f'{match.group(2)}.{end_date.year}', format_pattern)
+        start_date = datetime.strptime(f'{match.group(2)}.{current_year}', format_pattern)
+        if match.group(3):
+            end_date = datetime.strptime(match.group(3) + '.' + current_year, format_pattern)
+        else:
+            end_date = start_date
         current_date = start_date
         while current_date <= end_date:
             date_list.append(current_date)
