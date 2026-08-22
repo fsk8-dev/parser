de80120e7870a5b89045c0f65fa4624c4b264463
TimPulin <tigeronsun@gmail.com>
2026-08-22 14:17:03 +0300

    fix time parser in IcebergStachek

--- parsers/stachek_iceberg.py
+++ parsers/stachek_iceberg.py
@@ -12,7 +12,7 @@ from .classes.schedule_type import ScheduleType
 
 
 def get_day_schedule_list(text, sport_schedule_pattern):
-    time_pattern = r'\d{1,2}:\d{2}-\d{2}:\d{2}'
+    time_pattern = r'\d{1,2}:\d{2}-\d{1,2}:\d{2}'
     date_list = []
     day_schedule_list = []
     matches = re.findall(sport_schedule_pattern, text)
